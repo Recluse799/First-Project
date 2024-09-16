3731992892e09a204edf0f1c364cf0b342d2b5c9
Recluse799 <ImmanuelBraxton@Gmail.com>
2024-09-16 00:22:00 -0400

    Very slight change in the last few comments.

--- First Project.cpp
+++ First Project.cpp
@@ -17,27 +17,23 @@
 
 #include <iomanip>
 
-#include <string>
-
-#include <fstream>
-
 
 using namespace std;
 
 int main()
 {
-    double drinks = 16000;
+    double consumers  = 16500;
     double drinkPweek = 0.15;
     double critrusLvr = 0.58;
     double Anw1;
     double Anw2;
 
     cout << "A soft drink company recently surveyed 16,500 of its consumers.\n15% of them would purchase one or more energy drinks per week." << endl;
-    Anw1 = drinks * drinkPweek;
-    cout << "The number of people in that procentage that who would purchase one or more drinks per week was " << Anw1 << " customers.\n" << endl;
-    cout << "The company would also see a surge of 58% of it's consumers purchasing the citrus flavored drinks." << endl;
-    Anw2 = drinks * critrusLvr;
-    cout << "That would be " << Anw2 << " of there total costomers, purchasing the citrus flavor." << endl;
+    Anw1 = consumers  * drinkPweek;
+    cout << "The number of people with that precentage was " << Anw1 << " customers.\n" << endl;
+    cout << "The company would also see a surge of 58% of it's drinking consumers purchasing the citrus flavored drinks." << endl;
+    Anw2 = Anw1 * critrusLvr;
+    cout << "The total number of customers purchasing citrus flavor would be " << Anw2 << "." << endl;
 
     return 0;
 }
@@ -48,6 +44,7 @@ int main()
 * calculate
 * say more stuff
 * calculate again
+* say one last thing
 * end
 * 16,000 drink / 0.15 perch / 7 week /  0.58 citrus lovers.
 */
